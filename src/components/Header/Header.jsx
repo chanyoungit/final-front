e1192580c4c3f357e5cef0fb790c84f855999eb9
Chan Young Park <157675949+chanyoungit@users.noreply.github.com>
2024-10-23 16:41:15 +0900

    Update Header.jsx

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,6 +2,7 @@ import * as style from "./style/Header";
 import { useNavigate } from "react-router-dom";
 
 export const Header = ({ children, color, background}) => {
+    // eslint-disable-next-line no-unused-vars
     const navigation = useNavigate();
     return (
         <style.Layout color={color} background={background} >
